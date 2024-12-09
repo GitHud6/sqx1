2faf2f276c75eeb3f078841acd118f75d2068ff3
sunqixiu <2033144064@qq.com>
2024-12-09 16:27:01 +0800

    result to ^

--- hello.c
+++ hello.c
@@ -1,7 +1,14 @@
 #include "stdio.h"
-
+#include "string.h"
+unsigned int temp;
 int main()
 {
-	printf("hello world\n");
+	char arr[] = "$GNRMC,123BUIPOOIANSKLFUNSAUD\r\n";
+	int length = strlen(arr);
+	for(int i=0;i<length;i++)
+	{
+		temp ^= arr[i];
+	}
+	printf("hello world\nresult = %x",temp);
 	return 0;
 }
